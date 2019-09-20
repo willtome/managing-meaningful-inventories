231b4268a2e74eabe19757c22e69ee62aef332bf
AlanCoding <arominge@redhat.com>
2019-09-20 14:51:04 -0400

    rename with _script for clarity

--- 04_as_script/run.sh
+++ 04_as_script/run.sh
@@ -1,1 +1,1 @@
-SQLITE_DATABASE_PATH=hosts.db SQLITE_DATABASE_TABLE=hosts ansible-inventory -i sqlite.py --list --export
+SQLITE_DATABASE_PATH=hosts.db SQLITE_DATABASE_TABLE=hosts ansible-inventory -i sqlite_script.py --list --export
