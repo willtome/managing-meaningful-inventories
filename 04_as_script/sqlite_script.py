879d904e4c1fe91f5e547706af9ed1869645b9e5
AlanCoding <arominge@redhat.com>
2019-09-20 14:59:08 -0400

    condense script example for display

--- 04_as_script/sqlite_script.py
+++ 04_as_script/sqlite_script.py
@@ -3,28 +3,19 @@
 # How to use: set environment variables for input
 # SQLITE_DATABASE_PATH=hosts.db
 # SQLITE_DATABASE_TABLE=hosts
-
 import sqlite3
 import os
 import json
 
 
-r = {
-    '_meta': {
-        'hostvars': {}
-    }
-}
-
+r = {'_meta': {'hostvars': {}}}
 
 db_file_in = os.environ.get('SQLITE_DATABASE_PATH')
-
 if os.path.isabs(db_file_in):
     db_file = db_file_in
 else:
     db_file = os.path.join(os.path.dirname(__file__), db_file_in)
-
 db_table = os.environ.get('SQLITE_DATABASE_TABLE')
-
 conn = sqlite3.connect(db_file)
 c = conn.cursor()
 c.execute("SELECT * FROM {tn}" .format(tn=db_table))
